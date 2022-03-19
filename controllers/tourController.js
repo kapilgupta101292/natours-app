d58d1dbd6959bfacfddae5656b17c606ee6637fc
Kapil Gupta <kapilguptansit@gmail.com>
2022-03-19 05:47:49 +0000

    added middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,14 @@ const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkBody = (req, res, next) => {
+	if(!req.body.name || !req.body.price) {
+		return res.status(400).json({
+			status: 'fail',
+			message: 'Missing name or price'
+		})
+	}
+}
 exports.getAllTours = (req, res) => {
 	console.log(req.requestTime);
 	res.status(200).json({
