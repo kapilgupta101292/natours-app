e37cecc943eefa540006ca7271dcbec2539d6cb7
Kapil Gupta <kapilguptansit@gmail.com>
2022-03-19 06:36:30 +0000

    added config

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 //MIDDLEWARES
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
+
+
 app.use((req, res, next) => {
 	console.log('Hello from the middleware');
 	next();
@@ -29,8 +34,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-
-app.listen(port, () => {
-	console.log(`App running on the port ${port}...`);
-});
+module.exports = app;
