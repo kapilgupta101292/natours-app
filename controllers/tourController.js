5f6e23ac8022d2cf17abf639dd992936b907c158
Kapil Gupta <kapilguptansit@gmail.com>
2022-03-25 20:02:21 +0530

    added tours model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,25 +1,31 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel')
 
-const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.createTour = async (req, res) => {
+	try {
 
-exports.checkBody = (req, res, next) => {
-	if(!req.body.name || !req.body.price) {
-		return res.status(400).json({
+		const newTour = await Tour.create(req.body);
+		res.status(201).json({
+			status: 'success',
+			data: {
+				tour: newTour,
+			},
+		});
+	} catch(err) {
+		res.status(400).json({
 			status: 'fail',
-			message: 'Missing name or price'
+			message: err
 		})
 	}
-}
+};
+
 exports.getAllTours = (req, res) => {
 	console.log(req.requestTime);
 	res.status(200).json({
 		status: 'success',
-		results: tours.length,
-		data: {
-			tours,
-		},
+		// results: tours.length,
+		// data: {
+		// 	tours,
+		// },
 		requestedAt: req.requestTime,
 	});
 };
@@ -29,66 +35,62 @@ exports.getTour = (req, res) => {
 
 	const id = req.params.id * 1;
 
-	const tour = tours.find((el) => el.id === id);
-	if (!tour) {
-		res.status(404).json({
-			status: 'fail',
-			messase: 'Invalid Tour Id',
-		});
-		return;
-	}
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour,
-		},
-	});
+	// const tour = tours.find((el) => el.id === id);
+	// if (!tour) {
+	// 	res.status(404).json({
+	// 		status: 'fail',
+	// 		messase: 'Invalid Tour Id',
+	// 	});
+	// 	return;
+	// }
+	// res.status(200).json({
+	// 	status: 'success',
+	// 	data: {
+	// 		tour,
+	// 	},
+	// });
 };
 
-exports.createTour = (req, res) => {
-	const newId = tours[tours.length - 1].id + 1;
-	const newTour = Object.assign({ id: newId }, req.body);
-	tours.push(newTour);
-	fs.writeFile(
-		`${__dirname}/dev-data/data/tours-simple.json`,
-		JSON.stringify(tours),
-		(err) => {
-			res.status(201).json({
-				status: 'success',
-				data: {
-					tour: newTour,
-				},
-			});
-		}
-	);
-};
+
 
 exports.updateTour = (req, res) => {
-	if (req.params.id * 1 > tours.length) {
-		res.status(404).json({
-			status: 'fail',
-			message: 'Invalid tour Id',
-		});
-	}
+	// if (req.params.id * 1 > tours.length) {
+	// 	res.status(404).json({
+	// 		status: 'fail',
+	// 		message: 'Invalid tour Id',
+	// 	});
+	// }
 
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour: '<Updated tour here>',
-		},
-	});
+	// res.status(200).json({
+	// 	status: 'success',
+	// 	data: {
+	// 		tour: '<Updated tour here>',
+	// 	},
+	// });
 };
 
 exports.deleteTour = (req, res) => {
-	if (req.params.id * 1 > tours.length) {
-		res.status(404).json({
-			status: 'fail',
-			message: 'Invalid tour Id',
-		});
-	}
+	// if (req.params.id * 1 > tours.length) {
+	// 	res.status(404).json({
+	// 		if (req.params.id * 1 > tours.length) {
+	// 			// 	res.status(404).json({
+	// 			// 		status: 'fail',
+	// 			// 		message: 'Invalid tour Id',
+	// 			// 	});
+	// 			// }
+			
+	// 			// res.status(200).json({
+	// 			// 	status: 'success',
+	// 			// 	data: {
+	// 			// 		tour: '<Updated tour here>',
+	// 			// 	},
+	// 			// });	status: 'fail',
+	// 		message: 'Invalid tour Id',
+	// 	});
+	// }
 
-	res.status(204).json({
-		status: 'success',
-		data: null,
-	});
+	// res.status(204).json({
+	// 	status: 'success',
+	// 	data: null,
+	// });
 };
--- server.js
+++ server.js
@@ -18,23 +18,7 @@ mongoose.connect(DB, {
 })
 
 
-const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must have a name'],
-		unique: true
-	},
-	rating: {
-		type: Number,
-		default: 4.5
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price']
-	},
-})
 
-const Tour = mongoose.model('Tour', tourSchema);
 
 app.listen(port, () => {
 	console.log(`App running on the port ${port}...`);
